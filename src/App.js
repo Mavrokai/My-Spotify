a3bc3a1973d4fcd93c1001bb5f069a6d1de2e107
jonathanyoka <jonathan.yoka-lusavuku@epitech.eu>
2025-03-21 23:24:30 +0100

    ajout de la fonctionnaliter des albums

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import Accueil from "./components/accueil";
-import Navbar from "./components/Navbar";
-import ArtistDetails from "./components/detailsArtiste";
-import Albums from "./components/Album";
-import Sidebar from "./components/Sidebar";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importation des composants de React Router
+import { useState } from "react"; // Importation du hook useState pour gérer l'état local
+import Accueil from "./components/accueil"; // Composant Accueil
+import Navbar from "./components/Navbar"; // Composant Navbar
+import ArtistDetails from "./components/DetailsArtiste"; // Composant des détails de l'artiste
+import Albums from "./components/Album"; // Composant Albums
+import Sidebar from "./components/Sidebar"; // Composant Sidebar
+import "./App.css"; // Importation du fichier CSS
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState(""); // Hook d'état pour la recherche
 
   const handleSearch = (term) => {
-    setSearchTerm(term);
+    setSearchTerm(term); // Met à jour le terme de recherche
   };
 
   return (
     <Router>
-      <Navbar onSearch={handleSearch} /> {/* Passez la fonction handleSearch */}
-      <Sidebar /> 
+      <Navbar onSearch={handleSearch} />{" "}
+      {/* Passe la fonction de recherche à Navbar */}
+      <Sidebar /> {/* Composant Sidebar */}
       <div className="page-content">
         <Routes>
+          {/* Définition des différentes routes de l'application */}
           <Route path="/" element={<Accueil searchTerm={searchTerm} />} />
           <Route path="/detailsArtiste/:id" element={<ArtistDetails />} />
           <Route path="/Album" element={<Albums />} />
@@ -29,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; // Exporte le composant App
