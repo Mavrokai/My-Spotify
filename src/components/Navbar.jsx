f41b3acbc2401c22c00042d0e66b9d84f386dbdc
mavrokai <mavrokai.valentin.verscheure@epitech.eu>
2025-03-21 23:40:02 +0100

    [/] logo navbar Redirige vers le home / modification du Navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Navbar.css";
 
 function Navbar({ onSearch }) {
   return (
     <nav className="navbar">
       <div className="logo">
-        <h1>🎵 MySpotify</h1>
+        <Link to="/"><h1>🎵 MySpotify</h1></Link>
       </div>
       <div className="search-bar">
         <input
